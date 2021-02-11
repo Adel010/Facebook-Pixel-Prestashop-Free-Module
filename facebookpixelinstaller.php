64adac4884c7aba0d0c1dd08b281b1893eec32d4
Adel Alikeche <adel.alikeche.pro@gmail.com>
2021-02-11 18:33:37 +0100

    fix: InitiateCheckout and AddPaymentInfo events (only for PS version => 1.7.7.1)

--- facebookpixelinstaller.php
+++ facebookpixelinstaller.php
@@ -201,7 +201,7 @@ class Facebookpixelinstaller extends Module
                 ],
                 [
                     'type' => 'switch',
-                    'label' => $this->l('Use InitiateCheckout event'),
+                    'label' => $this->l('Use InitiateCheckout event (PS v1.7.7.1 or later only)'),
                     'name' => 'initiatecheckout_active',
                     'values' => array(
                         array(
@@ -218,7 +218,7 @@ class Facebookpixelinstaller extends Module
                 ],
                 [
                     'type' => 'switch',
-                    'label' => $this->l('Use AddPaymentInfo event'),
+                    'label' => $this->l('Use AddPaymentInfo event (PS v1.7.7.1 or later only)'),
                     'name' => 'addpaymentinfo_active',
                     'values' => array(
                         array(
